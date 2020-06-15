2e7528f7516a17bf5a6b8cb603a780f23d188376
haya14busa <haya14busa@gmail.com>
2020-06-15 19:29:16 +0000

    debug: test code ql

---
+++ codeqltest.go
@@ -0,0 +1,17 @@
+package reviewdog
+
+import (
+	"errors"
+	"net/http"
+	"regexp"
+)
+
+// https://github.com/github/codeql-go/blob/a88bf4c9fa495c2b9b3169853c96b89094fcdeed/ql/test/query-tests/Security/CWE-020/MissingRegexpAnchor/MissingRegexpAnchor.go
+func checkRedirect2(req *http.Request, via []*http.Request) error {
+	// BAD: the host of `req.URL` may be controlled by an attacker
+	re := "https?://www\\.example\\.com/"
+	if matched, _ := regexp.MatchString(re, req.URL.String()); matched {
+		return nil
+	}
+	return errors.New("Invalid redirect")
+}
